24d010a3d1d82703598b522c4a80dc08a1d0ffba
Rafael <rafa@blackmagicbox.com.br>
2019-09-04 03:04:37 +0200

    Fix the scrolling behavior when the app is loaded

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Post from './Post'
 import './Posts.scss';
 
 const Posts = ({posts}) => {
-
+    const endOfTheList = useRef(null);
+    const scrollToBottom = () => {
+        endOfTheList.current.scrollIntoView()
+    };
+    useEffect(scrollToBottom, [posts]);
     return (
         <div className="posts">
             <div className="posts-list">
                 {posts.map(post => <Post key={post.timestamp} {...post} />)}
             </div>
+            <div ref={endOfTheList}/>
         </div>
     );
 };
--- src/services/getMessagesService.js
+++ src/services/getMessagesService.js
@@ -2,5 +2,5 @@ import ServiceClient from './ServiceClient';
 
 export default function getMessages() {
     return ServiceClient('GET',
-        'https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/?token=9RxVxgFwBcKf');
+        'https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/?since=1521096352339&limit=10&token=9RxVxgFwBcKf');
 }
